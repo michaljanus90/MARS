bc0190ad047e6332d5b183719bf0faba80b9ee75
Michal <michal.janus.mj@gmail.com>
2018-09-05 17:45:53 +0200

    Fixed sorted by amount

--- src/SumByAccountReport.cpp
+++ src/SumByAccountReport.cpp
@@ -12,11 +12,11 @@
 #include <boost/bimap/multiset_of.hpp>
 #include "boost/bimap/list_of.hpp"
 #include "boost/bimap/vector_of.hpp"
-
+#include <functional>
 namespace
 {
     template< class MapType >
-    void print_map(const MapType & m)
+    void print_map_reverse(const MapType & m)
     {
         typedef typename MapType::const_reverse_iterator const_reverse_iterator;
         int i=0;
@@ -26,14 +26,36 @@ namespace
             i++;
         }
     }
+
+    template< class MapType >
+    void print_map(const MapType & m)
+    {
+        typedef typename MapType::const_iterator const_iterator;
+        int i=0;
+        for( const_iterator iter = m.begin(), iend = m.end(); iter != iend; ++iter )
+        {
+            std::cout << i << iter->second << " : " << iter->first << std::endl;
+            i++;
+        }
+    }
 }
 void SumByAccountReport::genrateReport(const std::vector<MoneyTransfer> &allTransfers, const std::vector<MoneyTransfer> &currentTransfers)
 {
-    boost::bimap<boost::bimaps::set_of<std::string> , boost::bimaps::vector_of<double>> transactionsByAccount;
+    typedef boost::bimap<boost::bimaps::set_of<std::string> , boost::bimaps::multiset_of<double,std::greater<double>>> bm_type;
+    bm_type transactionsByAccount;
 
     for (const auto& transfer : allTransfers)
     {
-        transactionsByAccount.left[transfer.fromAccount_] += transfer.amount_;
+        auto it = transactionsByAccount.left.find(transfer.fromAccount_);
+        if (it != transactionsByAccount.left.end())
+        {
+            transactionsByAccount.left.replace_data( it, (it->second + transfer.amount_) );
+        }
+        else
+        {
+            transactionsByAccount.insert(bm_type::value_type(transfer.fromAccount_, transfer.amount_));
+        }
     }
+
     print_map(transactionsByAccount.right);
 }
---
+++ tests/UT/SumByAccountReport_Test.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Michal on 03.09.2018.
+//
+
+#include "gtest/gtest.h"
+#include "SumByAccountReport.h"
+#include "MoneyTransfer.h"
+class SumByAccountReportFixure : public ::testing::Test
+{
+public:
+    SumByAccountReportFixure()
+    : sumByAccountReport_(std::make_unique<SumByAccountReport>())
+    {}
+//private:
+    std::unique_ptr<SumByAccountReport> sumByAccountReport_;
+};
+
+TEST_F(SumByAccountReportFixure, SumByAccountReport_Test1)
+{
+    MoneyTransfer moneyTransfer({"2014.01.02","12:00:14","67 1234 5678 0000 0000 1234 5678","68 1234 5678 0000 0000 1234 5678","123.45"});
+    auto moneyTransfer2 = moneyTransfer;
+    moneyTransfer2.fromAccount_ = "88 1234 5678 0000 0000 1234 5678";
+//    moneyTransfer2.amount_ = 2222222.22;
+    std::vector<MoneyTransfer> vec{moneyTransfer,moneyTransfer2,moneyTransfer,moneyTransfer};
+
+    sumByAccountReport_->genrateReport(vec,{});
+
+    EXPECT_EQ(1,2);
+}
